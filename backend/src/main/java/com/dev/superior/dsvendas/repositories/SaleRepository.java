ed69176dc7b84eec196e3c392f7600141cc2039f
EricaSugui <ericasugui@gmail.com>
2021-05-09 00:42:45 -0300

    Pagination

---
+++ backend/src/main/java/com/dev/superior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.dev.superior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.superior.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
